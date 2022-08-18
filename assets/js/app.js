1ccbc4cd94f4a12016f347cdd84e4a676c4f3c85
elnurxo <elnurykh@code.edu.az>
2022-08-18 10:12:17 +0400

    Modal Script Done

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,3 +17,25 @@ otherImages.forEach((image)=>{
     });
 });
 
+//Subscribe Modal Open
+let subscribeBtn = document.querySelector(".subscribe-btn");
+let subscribeModalWrapper = document.querySelector(".app--modal");
+let subscribeModal = document.querySelector(".app--modal__item");
+
+
+subscribeBtn.addEventListener("click",function(){
+    subscribeModalWrapper.style.visibility = "visible";
+    subscribeModalWrapper.style.opacity = "1";
+    subscribeModal.style.transform = "scale(1)";
+    subscribeModal.style.transition = ".4s linear";
+});
+
+//Subscribe Modal Close
+let closeModal = document.querySelector(".remove");
+
+closeModal.addEventListener("click",function(){
+    subscribeModalWrapper.style.visibility = "hidden";
+    subscribeModal.style.transform = "scale(0.01)";
+    subscribeModal.style.transition = "none";
+
+});
