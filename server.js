2abf6ca061cdaf0ba2d60b3c66121bb6cd4bef46
elnurxo <elnurykh@code.edu.az>
2022-08-21 20:07:07 +0400

    startup

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const axios = require('axios');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000 || 3001;
 const cors = require('cors');
 app.use(cors());
 
