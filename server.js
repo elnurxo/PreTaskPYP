4faf0ddd62337588ab733b230b38fda96dbb2f4f
elnurxo <elnurykh@code.edu.az>
2022-08-21 11:19:56 +0400

    bugs for deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 var http = require('http').Server(app);
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
     console.log('Listening to '+port);
